b68b78c13addf53d226f1b70233d46a08693be73
cmedina-git <4chmedina@gmail.com>
2020-09-28 18:16:42 -0400

    change message
    	modified:   app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Time to do some real work!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
