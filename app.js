64f1834051005c689a19905182e33a1388f1160e
cmedina-git <4chmedina@gmail.com>
2020-09-29 11:32:07 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Time to do some real work!'));
+app.get('/', (req, res) => res.send('This time lets get the party started!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
